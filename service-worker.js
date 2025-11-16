4ee5e24d16462eb22f4049b2665e160fbeec2b9c
SUSORH79 <SSTRABAJOSVARIOS@GMAIL.COM>
2025-11-16 16:39:04 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'op-app-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+  // Si luego añades CSS/JS externos, añádelos aquí.
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Devuelve del caché si existe, si no va a la red
+      return response || fetch(event.request);
+    })
+  );
+});
